f6222923ba54cdad46e05774f1316975d47c40e9
bozhen <undeading@Yandex.ru>
2020-04-20 23:00:29 +0300

    add register method and Db config

---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+const dbConfig = {
+    host: "mysql",
+    user: "root",
+    database: "game_market",
+    password: "12345",
+    dialect: "mysql",
+}
+
+ const connection = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.connection = connection;
+
+//Connect our Models////////////////////////////////
+db.user = require("../Models/user")(connection, Sequelize);
+
+////////////////////////////////////////////////////
+
+module.exports = db;
